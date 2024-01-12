16a29d5104f4f9b177123b1bf67fe77b8ba6498a
pranjeetkumar <mr.pran950@gmail.com>
2024-01-12 18:17:46 +0530

    initial project setup

---
+++ src/components/Menus/Menus.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Menus/Menus.css';
+
+const Menus = () => {
+  return (
+    <>
+    <div className="navbar-profile-pic">
+        <img src="./pran.jpg" alt='profile pic'/>
+    </div>
+    </>
+  )
+}
+
+export default Menus
